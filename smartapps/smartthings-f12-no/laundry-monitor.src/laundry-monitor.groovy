105b1a85f281b61b0ddf92ec0cbb79906961447e
Anders Sveen <anders@f12.no>
2017-05-30 11:40:48 +0200

    Just basic formatting

--- smartapps/smartthings-f12-no/laundry-monitor.src/laundry-monitor.groovy
+++ smartapps/smartthings-f12-no/laundry-monitor.src/laundry-monitor.groovy
@@ -18,44 +18,44 @@
 import groovy.time.*
 
 definition(
-  name: "Laundry Monitor",
-  namespace: "smartthings.f12.no",
-  author: "Anders Sveen",
-  description: "Using a switch with powerMonitor capability, monitor the laundry cycle and alert when it's done.",
-  category: "Green Living",
-  iconUrl: "https://s3.amazonaws.com/smartthings-device-icons/Appliances/appliances8-icn.png",
-  iconX2Url: "https://s3.amazonaws.com/smartthings-device-icons/Appliances/appliances8-icn@2x.png")
+        name: "Laundry Monitor",
+        namespace: "smartthings.f12.no",
+        author: "Anders Sveen",
+        description: "Using a switch with powerMonitor capability, monitor the laundry cycle and alert when it's done.",
+        category: "Green Living",
+        iconUrl: "https://s3.amazonaws.com/smartthings-device-icons/Appliances/appliances8-icn.png",
+        iconX2Url: "https://s3.amazonaws.com/smartthings-device-icons/Appliances/appliances8-icn@2x.png")
 
 
 preferences {
-  section ("When this device stops drawing power") {
-    input "meter", "capability.powerMeter", multiple: false, required: true
-    input "cycle_start_power_threshold", "number", title: "Start cycle when power consumption goes above (W)", required: true
-    input "cycle_end_power_threshold", "number", title: "Stop cycle when power consumption drops below (W) ...", required: true
-    input "cycle_end_wait", "number", title: "... for at least this long (min)", required: true
-  }
+    section("When this device stops drawing power") {
+        input "meter", "capability.powerMeter", multiple: false, required: true
+        input "cycle_start_power_threshold", "number", title: "Start cycle when power consumption goes above (W)", required: true
+        input "cycle_end_power_threshold", "number", title: "Stop cycle when power consumption drops below (W) ...", required: true
+        input "cycle_end_wait", "number", title: "... for at least this long (min)", required: true
+    }
 
-  section ("Send this message") {
-    input "message", "text", title: "Notification message", description: "Laudry is done!", required: true
-    input "phone", "phone", title: "SMS Notification", description: "Optional phone number to send to", required: false
-  }
+    section("Send this message") {
+        input "message", "text", title: "Notification message", description: "Laudry is done!", required: true
+        input "phone", "phone", title: "SMS Notification", description: "Optional phone number to send to", required: false
+    }
 }
 
 def installed() {
-  log.debug "Installed with settings: ${settings}"
+    log.debug "Installed with settings: ${settings}"
 
-  initialize()
+    initialize()
 }
 
 def updated() {
-  log.debug "Updated with settings: ${settings}"
+    log.debug "Updated with settings: ${settings}"
 
-  unsubscribe()
-  initialize()
+    unsubscribe()
+    initialize()
 }
 
 def initialize() {
-  subscribe(meter, "power", handler)
+    subscribe(meter, "power", handler)
 }
 
 def handler(evt) {
@@ -67,7 +67,7 @@ def handler(evt) {
     }
     // If power drops below threshold, wait for a few minutes.
     else if (state.cycleOn && latestPower <= cycle_end_power_threshold) {
-    	log.debug "Power below threshold. Scheduling end cycle"
+        log.debug "Power below threshold. Scheduling end cycle"
         runIn(cycle_end_wait * 60, cycleOff)
     }
 }
@@ -89,8 +89,8 @@ def cycleOff(evt) {
         sendPush(message)
         if (phone) {
             sendSms(phone, message)
-        }        
+        }
     } else {
-    	log.debug "Cycle continuing as power is above threshold."
+        log.debug "Cycle continuing as power is above threshold."
     }
 }
